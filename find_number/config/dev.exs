8fda9ef8596254d7c3eb0491714d7459eaa7921d
popo63301 <popo63301@gmail.com>
2019-11-16 12:41:52 +0100

    guides wip

--- find_number/config/dev.exs
+++ find_number/config/dev.exs
@@ -51,7 +51,7 @@ config :find_number, FindNumberWeb.Endpoint,
     patterns: [
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
-      ~r"lib/find_number_web/{live,views}/.*(ex)$",
+      ~r"lib/find_number_web/live/.*(ex)$",
       ~r"lib/find_number_web/templates/.*(eex)$"
     ]
   ]
